d1a2d97cc644ed8f307b20c2e1e9fff9d7657eac
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-06-26 13:30:42 +0200

    fix jogsaw application project

--- org.javacream.training.java10.jigsaw.application/src/module-info.java
+++ org.javacream.training.java10.jigsaw.application/src/module-info.java
@@ -7,7 +7,7 @@ import org.javacream.training.java10.jigsaw.publisher.StringSubscriber;
  * @author Administrator
  *
  */
-module org.javacream.training.java9.jigsaw.application {
-	requires org.javacream.training.java9.jigsaw.publisher;
+module org.javacream.training.java10.jigsaw.application {
+	requires org.javacream.training.java10.jigsaw.publisher;
 	uses StringSubscriber;
 }
