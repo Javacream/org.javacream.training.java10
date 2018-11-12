23484bc618fd45198c8759e6bc5bc3d9ae6c87ef
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-12 12:50:35 +0100

    introduce opens

--- java10/org.javacream.training.java1x.echo/src/org/javacream/training/java1x/echo/impl/EchoServiceImpl.java
+++ java10/org.javacream.training.java1x.echo/src/org/javacream/training/java1x/echo/impl/EchoServiceImpl.java
@@ -2,21 +2,19 @@ package org.javacream.training.java1x.echo.impl;
 
 import org.javacream.training.java1x.echo.api.EchoService;
 
-public class EchoServiceImpl implements EchoService
-{
+public class EchoServiceImpl implements EchoService {
 
-	{
-		System.out.println("CREATING ECHOSERVICEIMPL " + this);
-	}
-	@Override
-	public String echo(String message)
-	{
-		return new StringBuilder(message).reverse().toString();
-	}
+    {
+	System.out.println("CREATING ECHOSERVICEIMPL " + this);
+    }
 
-	public String echoIntern(String message)
-	{
-		return message;
-	}
+    @Override
+    public String echo(String message) {
+	return new StringBuilder(message).reverse().toString();
+    }
+
+    public String echoIntern(String message) {
+	return message;
+    }
 
 }
